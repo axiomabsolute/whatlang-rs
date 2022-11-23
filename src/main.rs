90e805ad06b5372b114a4725500beb1949a9b25d
Devon <axiomabsolute@gmail.com>
2022-11-22 16:07:03 -0800

    Implement `detect` GQL mutation.
    - Add async-graphql dependency
    - Add mutation to call `detect`
    - Add endpoint for mutation and playground

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    get, guard, post,
+    web::{self, Data},
+    App, HttpResponse, HttpServer, Responder,
+};
+use async_graphql::{http::GraphiQLSource, EmptySubscription, Object, Schema, SimpleObject};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
 use clap::{Parser, Subcommand};
 use whatlang::detect;
 
@@ -39,8 +45,54 @@ async fn detect_endpoint(req_body: String) -> impl Responder {
         .map_or_else(|v| v, |v| v)
 }
 
+async fn index_graphiql() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(
+            GraphiQLSource::build()
+                .endpoint("http://127.0.0.1:8080/gql")
+                .finish(),
+        )
+}
+
+async fn index_graphql(
+    schema: web::Data<Schema<QueryRoot, MutationRoot, EmptySubscription>>,
+    req: GraphQLRequest,
+) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+struct QueryRoot;
+#[Object]
+impl QueryRoot {
+    async fn howdy(&self) -> &'static str {
+        "partner"
+    }
+}
+
+#[derive(Clone, SimpleObject)]
+struct DetectResult {
+    lang: String,
+    script: String,
+    confidence: f64,
+}
+
+struct MutationRoot;
+#[Object]
+impl MutationRoot {
+    async fn detect(&self, text: String) -> DetectResult {
+        let info = detect(&text).expect("Should be able to detect");
+        DetectResult {
+            lang: info.lang().eng_name().to_owned(),
+            script: info.script().name().to_owned(),
+            confidence: info.confidence(),
+        }
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
     let args = Cli::parse();
 
     match args.command {
@@ -52,11 +104,19 @@ async fn main() -> std::io::Result<()> {
             Ok(())
         }
         Commands::Serve { path, port } => {
-            println!("Serve!! :{}, save to {}", port, path);
-            HttpServer::new(|| App::new().service(health).service(detect_endpoint))
-                .bind(("127.0.0.1", port))?
-                .run()
-                .await
+            println!("Serve :{}, stats to {}", port, path);
+            HttpServer::new(|| {
+                let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription).finish();
+                App::new()
+                    .app_data(Data::new(schema.clone()))
+                    .service(health)
+                    .service(detect_endpoint)
+                    .service(web::resource("/gql").guard(guard::Post()).to(index_graphql))
+                    .service(web::resource("/gql").guard(guard::Get()).to(index_graphiql))
+            })
+            .bind(("127.0.0.1", port))?
+            .run()
+            .await
         }
     }
 }
